457a89f54d2c683a51a17278299afd46d5f99f43
Anish Bhateja <anishbhateja99@gmail.com>
2020-10-03 19:25:04 +0530

    Added CRUD routes

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
 
+//SENDING ALL TOURS
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -28,10 +29,33 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+//SENDING PARTICULAR TOUR
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1; //coverting string into number
+  const tour = tours.find((ele) => {
+    return ele.id === id;
+  });
+  //   if (id > tours.length - 1)
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+//ADDING NEW TOUR
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours.length;
-  console.log(req.body);
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = Object.assign({ id: newId }, req.body); //Object.assign merges 2 object to form 1 new object
+
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
@@ -45,6 +69,40 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+//UPDATE TOUR
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length - 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: `Tour ${id} updated!`,
+    },
+  });
+});
+
+//DELETE TOUR
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length - 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
 
 const port = 3000;
