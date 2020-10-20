c91c8dede619188e8c0935682dbe5ee91aebe762
Anish Bhateja <anishbhateja99@gmail.com>
2020-10-20 17:52:00 +0530

    Added security features -rate-limit,helmet,mongo santize,xss,hpp

--- app.js
+++ app.js
@@ -2,30 +2,73 @@ const express = require('express');
 
 const app = express();
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSantize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-//1) MIDDLEWARES
+//1) GLOBAL MIDDLEWARES
+//app.use always requires a function not a function call
+
+// SET SECURITY HTTP headers
+app.use(helmet()); //helmet() return a function which in turn is a midlleware function
+
+//Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); //LOGGER
 }
-app.use(express.json()); //middleware, PUTS DATA of incomming request(JSON) in it's body (req.body)
-app.use(express.static(`${__dirname}/public`)); //middleware that allows us to access static files,when nothing matches the route
+
+//Limit request from API
+const limiter = rateLimit({
+  //rate limit returns us a middleware
+  max: 500,
+  windowMs: 60 * 60 * 1000, //all 100 request in 1 hour window per IP address
+  message: 'Too many requests from this IP, Please try again in an hour',
+});
+
+app.use('/api', limiter); //rate limit returns us a middleware that we're using
+
+//Body Parser, PUTS DATA of incomming request(JSON) in it's body (req.body)
+app.use(express.json({ limit: '10kb' })); // will not body larger than 10 KB
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSantize()); //will look into all req.body,req.params and remove all $signs operators etc
+
+//Data sanitization against XSS (cross site scripting)
+app.use(xss()); //clean any input with malicious html javascript in it
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+//Serving static files,when nothing matches the route
+app.use(express.static(`${__dirname}/public`));
+
+//Test Middleware
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 🙄');
-  //console.log(req.headers);
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+//Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,18 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    //secure: false, //cookie will only be sent over https
+    httpOnly: true, // client receive cookie, stores it and sends it with every response
+  };
+  if (process.env.NODE_ENV === 'production ') {
+    cookieOptions.secure = true; //cookie will only be sent over https
+  }
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined;
   res.status(statusCode).json({
     status: 'success',
     token,
